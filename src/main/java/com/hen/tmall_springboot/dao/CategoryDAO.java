37d8d84117cced73bde9d860c1eea734138f52c9
Zhangqiwu-hen <1102925457@qq.com>
2021-01-27 04:58:42 +0800

    2021/1/27

--- src/main/java/com/hen/tmall_springboot/dao/CategoryDAO.java
+++
@@ -1,7 +0,0 @@
-package com.hen.tmall_springboot.dao;
-
-import com.hen.tmall_springboot.pojo.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDAO extends JpaRepository<Category, Integer> {
-}
--- src/main/java/com/hen/tmall_springboot/es/ProductESDAO.java
+++
@@ -1,7 +0,0 @@
-package com.hen.tmall_springboot.es;
-
-import com.hen.tmall_springboot.pojo.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductESDAO extends ElasticsearchRepository<Product, Integer> {
-}
